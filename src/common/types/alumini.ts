41cf5129ba72882d43d20dc0d9967fb076ebaaa6
freshwaterlake <118621710+freshwaterlake@users.noreply.github.com>
2022-11-19 23:23:34 +0530

    Test

--- src/common/types/alumini.ts
+++ src/common/types/alumini.ts
@@ -6,4 +6,31 @@ export type BasicInformation = {
     mobileCountryCode: number;
     mobile: number;
     class12CompletionYear: number;
+    gapAfter12: string;
+    presentStatusCode: string;
+    residingCountryCode: string;
+    residingCityCode:String;
+    headline: string;
 };
+
+export type AdditionalInformation = {  
+    addressLine1: string;
+    addressLine2: String;
+    locality: String;
+    pin:string;
+    year12Cirriculum: string;
+}
+
+export type EducationalBackground = {
+    UGProgramTypeCode: string;
+    collegeCode: string;
+    fieldOfStudyCode: string;
+    startDateYear: number;
+    startDateMonth: number;
+    completionDateYear: number;
+    completionDateMonth: number;
+    receiveScholorship: string;
+    gradingSystemCode: string;
+    grade: string;
+    appearForEntranceExam: string;
+}
